1e14d9752d066716291d847b0df4b48e515b38a9
Jandrell Ian L. Formoso <jandrell.formoso@gmail.com>
2019-07-24 22:19:19 +0800

    Added comments

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -8,12 +8,12 @@ import { rootSaga } from "../sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 
+// exporting a createStore function which sets up the store and saga middleware
+// you can add other functions here like redux-persist and then return it with store
+// Ex. return { store, persistStore };
 export default () => {
-  const store = createStore(
-    reducers,
-    // TODO: SAGA - add root saga middleware
-    applyMiddleware(sagaMiddleware),
-  );
+  const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+  // run sagaMiddleware after store creation.
   sagaMiddleware.run(rootSaga);
   return store;
 };
